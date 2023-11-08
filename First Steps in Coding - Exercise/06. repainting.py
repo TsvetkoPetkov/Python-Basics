b022f157d1abf715fb78aacc2bf4d5f56c9d443a
TsvetkoPetkov23 <115669197+TsvetkoPetkov@users.noreply.github.com>
2023-11-08 21:05:19 +0200

    Update and rename 06. Repainting to 06. repainting.py

--- First Steps in Coding - Exercise/06. repainting.py
+++ First Steps in Coding - Exercise/06. repainting.py
@@ -11,4 +11,5 @@ thinner_sum = thinner * 5
 sum_for_materials = nylon_sum + paint_sum + thinner_sum + 0.4
 money_for_workers = (sum_for_materials * 30/100) * hours
 total_sum = sum_for_materials + money_for_workers
+
 print(total_sum)
