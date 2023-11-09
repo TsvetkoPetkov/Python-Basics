f158338dfc4f45abd1cd8324417cd72a1cd1bc33
TsvetkoPetkov23 <115669197+TsvetkoPetkov@users.noreply.github.com>
2023-11-09 23:28:47 +0200

    Update and rename 07. Trekking Mania to 07. trekking_mania.py

--- For Loop - Exercise/07. trekking_mania.py
+++ For Loop - Exercise/07. trekking_mania.py
@@ -2,22 +2,23 @@ n_groups = int(input())
 
 people_sum = 0
 
-muss_people = 0
+musala_people = 0
 monblan_people = 0
-kilim_people = 0
+kilimandjaro_people = 0
 k2_people = 0
 everest_people = 0
 
 for i in range(1, n_groups+1):
     group = int(input())
+    
     if group <= 5:
-        muss_people += group
+        musala_people += group
         people_sum += group
     elif 6 <= group <= 12:
         monblan_people += group
         people_sum += group
     elif 13 <= group <= 25:
-        kilim_people += group
+        kilimandjaro_people += group
         people_sum += group
     elif 26 <= group <= 40:
         k2_people += group
@@ -26,14 +27,14 @@ for i in range(1, n_groups+1):
         everest_people += group
         people_sum += group
 
-muss_percent = muss_people / people_sum * 100
+musala_percent = musala_people / people_sum * 100
 monblan_percent = monblan_people / people_sum * 100
-kilim_percent = kilim_people / people_sum * 100
+kilimandjaro_percent = kilimandjaro_people / people_sum * 100
 k2_percent = k2_people / people_sum * 100
 everest_percent = everest_people / people_sum * 100
 
-print(f"{muss_percent:.2f}%")
+print(f"{musala_percent:.2f}%")
 print(f"{monblan_percent:.2f}%")
-print(f"{kilim_percent:.2f}%")
+print(f"{kilimandjaro_percent:.2f}%")
 print(f"{k2_percent:.2f}%")
 print(f"{everest_percent:.2f}%")
