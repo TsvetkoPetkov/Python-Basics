efddc50b6443ec9d3abed8789485649ffbee8c51
TsvetkoPetkov23 <115669197+TsvetkoPetkov@users.noreply.github.com>
2023-11-10 21:04:53 +0200

    Update and rename 02. Equal Sums Even Odd Position to 02. equal_sums_even_odd_position.py

--- Nested Loops - Exercise/02. equal_sums_even_odd_position.py
+++ Nested Loops - Exercise/02. equal_sums_even_odd_position.py
@@ -6,6 +6,7 @@ for i in range(number_one, number_two+1):
     str_num = str(i)
     even_sum = 0
     odd_sum = 0
+    
     for j in range(len(str_num)):
         if j % 2 == 0:
             even_sum += int(str_num[j])
